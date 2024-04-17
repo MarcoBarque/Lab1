e30c9f74db994ca9261db49cdde30880bb7d8d2b
MarcoBarque <166959353+MarcoBarque@users.noreply.github.com>
2024-04-16 20:33:53 -0600

    Add files via upload

--- Lab1/main.cpp
+++ Lab1/main.cpp
@@ -1,13 +1,14 @@
 #include <iostream>
 #include <cmath>
 #include "complex.hpp"
+#include <iomanip>
 
 int main(){
 	Complex a;
 	std::cout << "Default Constructor" << std::endl;
 	a.Disp();
 
-	std::cout<< "" << std::endl;
+	std::cout<< std::endl;
 	double real;
 	double imag;
 	std::cout<< "Enter the real part of the complex number wanted: ";
@@ -19,37 +20,37 @@ int main(){
 	std::cout << "Personalized Constructor" << std::endl;
 	b.Disp();
 	
-	std::cout<< "" << std::endl;
+	std::cout<< std::endl;
 	Complex c = a;
 	c.Disp();
 
-	std::cout<< "" << std::endl;
+	std::cout<< std::endl;
 	c = Complex(3.0,2.0);
 	c.Disp();		
 
-	std::cout << "The complex's norm is " << c.Norm() << std::endl;
+	std::cout << "The complex's norm is " << std::fixed << std::setprecision(2) << c.Norm() << std::endl;
 
-	std::cout<< "" << std::endl;
+	std::cout<< std::endl;
 	std::cout << "By conjugating we get: " <<std::endl;
 	c.Conjugate();
 	c.Disp();
 
-	std::cout<< "" << std::endl;
+	std::cout<< std::endl;
 	Complex d = b + c;
 	std::cout << "Sum(+) of complexes" << std::endl;
 	d.Disp();
 
-	std::cout<< "" << std::endl;
+	std::cout<< std::endl;
 	Complex e = b - c;
         std::cout << "Substraction(-) of complexes" << std::endl;
         e.Disp();
 
-	std::cout<< "" << std::endl;
+	std::cout<< std::endl;
 	Complex f  = b * c;
         std::cout << "Multiplication(*) of complexes" << std::endl;
         f.Disp();
 
-	std::cout<< "" << std::endl;
+	std::cout<<std::endl;
 	Complex g = b/c;
 	std::cout << "Division(/) of complexes" << std::endl;
 	g.Disp();
