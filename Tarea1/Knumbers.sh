e64ce7fc2fd2c6f040916ebbbd2d4ee119c9987d
AndresEsCh <130942844+AndresEsCh@users.noreply.github.com>
2024-04-22 21:15:16 -0600

    Add files via upload

--- Tarea1/Knumbers.sh
+++ Tarea1/Knumbers.sh
@@ -1,5 +1,27 @@
 #!/bin/bash
 
+<<<<<<< HEAD
+times_f=4 #iteraciones de archivos (files)
+count_f=0 #iterador de archivos
+times_r=100 #iteraciones de randoms
+count_r=1 #iterador de randoms
+
+while [ "$count_f" -le $times_f ] #itera de 0 a 4 para los archivos
+do
+        count_r=1 #reinicia el iterador de randoms
+        while [ "$count_r" -le $times_r ] #itera de 1 a 100 para tener 1000 randoms
+        do
+                if [ "$count_r" -eq 1 ] #Chequea si es el primero
+                then
+                        bash random.sh | head -13 | tail -10 > rand_"$count_f".dat #reescribe archivo
+                else
+                        bash random.sh | head -13 | tail -10 >> rand_"$count_f".dat #agrega al archivo
+                fi
+                let "count_r += 1" #Incrementa count_r
+        done
+        let "count_f += 1" #Incrementa count_r
+done
+=======
 TIMES_r=100
 TIMES_f=4
 count_f=0
@@ -19,5 +41,6 @@ do
 	done
 	let "count_f += 1"
 done	
+>>>>>>> 359323a12aaa7fb5b5ec0e621ae5d94e074753d5
 
 exit 0
