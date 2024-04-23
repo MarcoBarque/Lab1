9a7fb52f2553851e593fb3eabcade5b27d3bc0b6
AndresEsCh <130942844+AndresEsCh@users.noreply.github.com>
2024-04-22 21:20:03 -0600

    Update Knumbers.sh

--- Tarea1/Knumbers.sh
+++ Tarea1/Knumbers.sh
@@ -1,6 +1,5 @@
 #!/bin/bash
 
-<<<<<<< HEAD
 times_f=4 #iteraciones de archivos (files)
 count_f=0 #iterador de archivos
 times_r=100 #iteraciones de randoms
@@ -21,26 +20,5 @@ do
         done
         let "count_f += 1" #Incrementa count_r
 done
-=======
-TIMES_r=100
-TIMES_f=4
-count_f=0
-
-while [ "$count_f" -le $TIMES_f ]
-do
-	count_r=1
-	while [ "$count_r" -le $TIMES_r ]
-	do
-		if [ "$count_r" -eq 1 ]
-		then 
-			bash random.sh | head -13 | tail -10 >  rand_"$count_f".dat
-		else
-			bash random.sh | head -13 | tail -10 >>  rand_"$count_f".dat
-		fi
-		let "count_r += 1"	
-	done
-	let "count_f += 1"
-done	
->>>>>>> 359323a12aaa7fb5b5ec0e621ae5d94e074753d5
 
 exit 0
