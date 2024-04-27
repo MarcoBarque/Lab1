e32963434ac0a903124bbaa7c12a157e066fb6e5
AndresEsCh <130942844+AndresEsCh@users.noreply.github.com>
2024-04-26 21:34:39 -0600

    Update k_numbers.sh

--- Tarea1/k_numbers.sh
+++ Tarea1/k_numbers.sh
@@ -1,11 +1,11 @@
 #!/bin/bash
 
-times_f=4 #iteraciones de archivos (files)
+times_f=${1} #iteraciones de archivos (files)
 count_f=0 #iterador de archivos
 times_r=100 #iteraciones de randoms
 count_r=1 #iterador de randoms
 
-while [ "$count_f" -le $times_f ] #itera de 0 a 4 para los archivos
+while [ "$count_f" -le $times_f ] #itera de 0 a i (times_f) para los archivos
 do
         count_r=1 #reinicia el iterador de randoms
         while [ "$count_r" -le $times_r ] #itera de 1 a 100 para tener 1000 randoms
