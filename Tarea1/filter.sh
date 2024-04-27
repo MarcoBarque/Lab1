a54caf5eb73e8bcdc53d43d5624e6e14f6aaa409
AndresEsCh <130942844+AndresEsCh@users.noreply.github.com>
2024-04-26 21:12:37 -0600

    Update filter.sh

--- Tarea1/filter.sh
+++ Tarea1/filter.sh
@@ -1,19 +1,19 @@
 #!/bin/bash
 
+#Impresión de la lista filtrada por nombre, dominio y compañía
 echo -e "Lista de James, con correo .org en Ad Astra \n"  
 grep ^James usuarios.csv | grep .org, | grep "Ad Astra" 
-#Impresión de la lista filtrada por nombre, dominio y compañía
-                                                        
-echo -e "\nLista de James y Paul, con apellido empezado en J o S en Ad Astra \n"
-grep -e ^"James,J" -e ^"Paul,J" -e "James,S" -e "Paul,S"  usuarios.csv | grep "Ad Astra"
-#Impresión de la lista filtrada por nombre y letra inicial del apellido 
 
-echo -e "\n Numero de usuarios cuyo nombre empieza con J \n"
+#Impresión de la lista filtrada por nombre y letra inicial del apellido 
+echo -e "\n\nLista de James y Paul, con apellido empezado en J o S en Ad Astra \n"
+grep -e ^"James,J" -e ^"Paul,J" -e "James,S" -e "Paul,S"  usuarios.csv | grep "Ad Astra"
+
+#Impresión de la cantidad de usuarios con nombre que inicia con J y su lista
+echo -e "\n\nNumero de usuarios cuyo nombre empieza con J \n"
 grep ^"J" usuarios.csv | wc -l
 grep ^"J" usuarios.csv > usuariosJ.csv
-#Impresión de la cantidad de usuarios con nombre que inicia con J y su lista
 
-sed -i 's/mastercard/Mastercard/g' usuarios.csv 
 #Cambio de instancias "mastercard" por "Mastercard"
+sed -i 's/mastercard/Mastercard/g' usuarios.csv 
 
 exit 0
